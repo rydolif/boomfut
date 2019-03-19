86431a826c792d6890d459cffcb1d8fd5607eebf
rudolif <rudolifrudolif@gmail.com>
2019-03-19 19:46:47 +0200

    page 6

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,27 @@
 $(function() {
 
+//------------------------------parks---------------------------
+  var swiper = new Swiper('.packs__slider', {
+    slidesPerView: 4,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      // when window width is <= 320px
+      480: {
+        slidesPerView: 1,
+      },
+      // when window width is <= 480px
+      767: {
+        slidesPerView: 2,
+      },
+      // when window width is <= 640px
+      992: {
+        slidesPerView: 3,
+      }
+    }
+  });
 
 //------------------------------acardeon---------------------------
   $(".faq__item_content").slideUp("slow");
@@ -26,59 +48,6 @@ $(function() {
     $('header').toggleClass('header--menu');
   });
 
-// //-------------------------------попандер---------------------------------------
-//   $('.modal').popup({transition: 'all 0.3s'});
-
-// //------------------------------------form-------------------------------------------
-//   $('input[type="tel"]').mask('+0 (000) 000-00-00');
-
-//   jQuery.validator.addMethod("phoneno", function(phone_number, element) {
-//      return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
-//   }, "Введите Ваш телефон");
-
-//   $(".form").each(function(index, el) {
-//     $(el).addClass('form-' + index);
-
-//     $('.form-' + index).validate({
-//       rules: {
-//         phone: {
-//           required: true,
-//           phoneno: true
-//         },
-//         name: 'required',
-//       },
-//       messages: {
-//         name: "Введите Ваше имя",
-//         phone: "Введите Ваш телефон",
-//       },
-//       submitHandler: function(form) {
-//         var t = {
-//           name: jQuery('.form-' + index).find("input[name=name]").val(),
-//           phone: jQuery('.form-' + index).find("input[name=phone]").val(),
-//           subject: jQuery('.form-' + index).find("input[name=subject]").val()
-//         };
-//         ajaxSend('.form-' + index, t);
-//       }
-//     });
-
-//   });
-
-//   function ajaxSend(formName, data) {
-//     jQuery.ajax({
-//       type: "POST",
-//       url: "sendmail.php",
-//       data: data,
-//       success: function() {
-//         $(".modal").popup("hide");
-//         $("#thanks").popup("show");
-//         setTimeout(function() {
-//           $(formName).trigger('reset');
-//         }, 2000);
-//       }
-//     });
-//   }
-
-
 //----------------------------------------fixed----------------------------------
   $(window).scroll(function(){
       if($(this).scrollTop()>20){
@@ -95,34 +64,8 @@ $(function() {
       var id  = $(this).attr('href'),
           top = $(id).offset().top;
       $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
-  //--------------------закриття меню при кліку на ссилку якоря--------------------
-     // $('.hamburger').removeClass('hamburger--active');
-     // $('.header-menu').removeClass('header-menu');
-     // $('.header--active').removeClass('header--active');
-     // $('.nav--active').removeClass('nav--active');
-
   });
 
-  // //-------------------------------анімація цифр---------------------------------------
-  //   var show = true;
-  //   var countbox = ".about-statistics__container";
-  //   $(window).on("scroll load resize", function () {
-  //       if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-  //       var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-  //       var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-  //       var w_height = $(window).height(); // Высота окна браузера
-  //       var d_height = $(document).height(); // Высота всего документа
-  //       var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-  //       if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-  //           $('.about-statistics__item h3').spincrement({
-  //               thousandSeparator: "",
-  //               duration: 2000
-  //           });
-  //           show = false;
-  //       }
-  //   });
-
-  
 });
 
 //----------------------------------------preloader----------------------------------
