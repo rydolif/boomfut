472a7ec81e151d149bb53d5e754dfd64043e7806
rudolif <rudolifrudolif@gmail.com>
2019-03-20 10:30:02 +0200

    page 10 card

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -26,19 +26,16 @@ $(function() {
     },
     breakpoints: {
       // when window width is <= 320px
-      320: {
+      480: {
         slidesPerView: 1,
-        spaceBetween: 10
       },
       // when window width is <= 480px
-      480: {
+      767: {
         slidesPerView: 2,
-        spaceBetween: 20
       },
       // when window width is <= 640px
       992: {
         slidesPerView: 3,
-        spaceBetween: 30
       }
     }
   });
